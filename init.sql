ae428be2fe1ae7146af2c806c4d304aff4338b0f
Josep Escoda <josep.escoda@lacetania.cat>
2022-07-26 09:37:11 +0200

    Inicialitzacio BD Kubernetes

---
+++ init.sql
@@ -0,0 +1,17 @@
+DROP DATABASE IF EXISTS bdaplicacio;
+CREATE DATABASE bdaplicacio;
+USE bdaplicacio;
+
+CREATE TABLE usuaris(
+id		INT(50) NOT NULL,
+nom		VARCHAR(80) NOT NULL,
+usuari		VARCHAR(50)NOT NULL,
+password	VARCHAR(25)NOT NULL,
+PRIMARY KEY(id));
+
+
+
+
+INSERT INTO usuaris VALUES(1,'Josep Escoda','josep','password');
+INSERT INTO usuaris VALUES(1,'Profe MP08','profe','password');
+

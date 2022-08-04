3b5a5db6d5e33f04be60c4ec8d43809b856fa0ab
Josep Escoda <josep.escoda@lacetania.cat>
2022-08-04 17:28:27 +0200

    Eliminacio COLLATE MySQL

--- db.sql
+++ db.sql
@@ -16,6 +16,6 @@ CREATE TABLE usuarios (
   username varchar(45) NOT NULL,
   password varchar(50) NOT NULL,
   PRIMARY KEY (username)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 INSERT INTO usuarios VALUES ("josep","1234567");

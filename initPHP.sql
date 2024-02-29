676f796b90a7e1f280daf38cc600980b070cce8b
josep-escoda <69038191+josep-escoda@users.noreply.github.com>
2024-02-29 08:55:07 +0100

    Update initPHP.sql

--- initPHP.sql
+++ initPHP.sql
@@ -1,13 +1,5 @@
 CREATE DATABASE p1docker;
-
 USE p1docker;
-
-DROP TABLE IF EXISTS `productes`;
-CREATE TABLE `productes` (
-  `idp` int NOT NULL,
-  `nomarticle` varchar(45) NOT NULL,
-  `preu` float DEFAULT NULL,
-  PRIMARY KEY (`idp`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-INSERT INTO `productes` VALUES (1,'Cargol del 7',0.1),(2,'Auriculars trencats',1.5),(3,'Somachigun',650.8),(4,'Plasters',5.99),(5,'Paquet Celtas',3.6);
+DROP TABLE IF EXISTS productes;
+CREATE TABLE productes (idp INT NOT NULL, nomarticle VARCHAR(45) NOT NULL, preu FLOAT DEFAULT 0.0 NULL, PRIMARY KEY (idp)) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+INSERT INTO productes VALUES (1,'Cargol del 7',0.1),(2,'Auriculars trencats',1.5),(3,'Somachigun',650.8),(4,'Plasters',5.99),(5,'Paquet Celtas',3.6);
